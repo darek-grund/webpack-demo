c130ab8d5d0a68fbfad9294a6054599a98893d63
Darek Grund <darek.grund@gmail.com>
2019-11-16 19:08:59 +0100

    Code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: {
         app: './src/index.js',
+        another: './src/another-module.js',
         print: './src/print.js',
     },
     devtool: 'inline-source-map',
@@ -22,5 +23,10 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-    }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
 };
